30652cde7156a9cd05dbe10ba815d52f9cf16cb6
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-08-31 19:43:45 +0600

    Two sum bruteforce

---
+++ leetcode_blind75/Array/twoSub_bruteforce.cpp
@@ -0,0 +1,45 @@
+// two sum leetcode bruteforce solution
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+#define nl "\n"
+
+int two_sum_bruteforce(int nums, int target, vector<int>& arr) {
+    int first,second,sum;
+    for(int i = 0; i < nums; i++) {
+        first = arr[i];
+        for(int j = 1; j < nums; j++) {
+            second = arr[j];
+            sum = first + second;
+            if(sum == target) {
+                cout << i << " " << j;
+                
+            }
+            break;
+        }
+        
+    }
+
+}
+
+int main() {
+    fast_io();
+
+    int nums,target;
+    cin >> nums >> target;
+        
+    vector<int> arr(nums);
+
+    for(int i = 0; i < nums; i++) cin >> arr[i];
+
+
+
+
+    two_sum_bruteforce(nums,target,arr);
+
+
+
+}
