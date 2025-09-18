5cfaa3829b785701a3448893aec5c4ad3766646e
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-09-18 23:56:12 +0600

    optimized solution

--- leetcode_blind75/String/longestRepeatingChar.cpp
+++ leetcode_blind75/String/longestRepeatingChar.cpp
@@ -24,7 +24,27 @@ int characterReplacement(string s,int k) {
 }
 
 int charReplacementOptimized(string s,int k) {
-    
+   
+    int cnt[26] = {0};
+    int left = 0;
+    int best = 0;
+    int maxFreq = 0; 
+
+    for (int right = 0; right < (int)s.size(); ++right) {
+        int r = s[right] - 'A';
+        maxFreq = max(maxFreq, ++cnt[r]);
+
+        
+        while ((right - left + 1) - maxFreq > k) {
+            int l = s[left] - 'A';
+            --cnt[l];
+            ++left;
+            
+        }
+
+        best = max(best, right - left + 1);
+    }
+    return best;
 }
 
 int main() {
@@ -35,5 +55,5 @@ int main() {
     int k;
     // Expect input like: ABAB 2
     if (!(cin >> s >> k)) return 0;
-    cout << characterReplacement(s, k) << '\n';
+    cout << charReplacementOptimized(s, k) << '\n';
 }
