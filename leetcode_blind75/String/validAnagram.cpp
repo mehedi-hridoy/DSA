6a47b89f6b217c3c1eb082af50dac1474fa311fa
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-09-19 22:02:51 +0600

    anagram optimized

--- leetcode_blind75/String/validAnagram.cpp
+++ leetcode_blind75/String/validAnagram.cpp
@@ -1,16 +1,45 @@
 // valid anagram leetcode 242
 #include<bits/stdc++.h>
 using namespace std;
+
+// basic version 
 bool validAnagram(string s, string t) {
     sort(s.begin(), s.end());
     sort(t.begin(), t.end());
     return s == t;
 }
+
+
+// optimized
+ bool isAnagram(string s, string t) {
+         if(s.size() != t.size()) {
+            return false;
+         }
+
+         int freq[26] = {0};
+
+         for(int i = 0 ; i < s.size(); i ++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+         }
+
+        for(int i =0; i < 26; i++) {
+            if(freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+        
+}
+
 int main() {
 
     string s,t;
     cin >> s >> t; 
-    bool ans = validAnagram(s,t);
+    // bool ans = validAnagram(s,t);
+    bool ans = isAnagram(s,t);
     if(ans) {
         cout << "true" << endl;
     } else {
