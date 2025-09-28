8e9083a625fdf6d89bcbbb2ccf7c89bc208ec668
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-09-28 21:41:09 +0600

    leetcode 1342 reduce number to zero

---
+++ 07_math/01math.cpp
@@ -0,0 +1,23 @@
+// 1342. Number of Steps to Reduce a Number to Zero leetcode 
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfSteps(int num) {
+        int c = 0;
+        while(num != 0) {
+            if(num % 2 == 0 ) {
+                num = num / 2;
+                c++;
+            } else {
+                num = num - 1;
+                c++;
+            }
+        }
+        return c;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << numberOfSteps(n);
+    
+}
